62cf4b9183a1f6006177afb30531329bbc6a4c71
bestdevelope <leabouillot7@gmail.com>
2024-07-13 18:16:59 +0200

    correction footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+const Footer = () => {
+  return <div>Footer</div>;
+};
 
-const Footer = props => {
-  return (
-    <div>Footer</div>
-  )
-}
-
-Footer.propTypes = {}
-
-export default Footer
+export default Footer;
