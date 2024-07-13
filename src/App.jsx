c9a2b217cb37decae073eb65f1600582cbdf75f7
bestdevelope <leabouillot7@gmail.com>
2024-07-13 18:13:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import Portfolio from "./components/Portfolio";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Services />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
